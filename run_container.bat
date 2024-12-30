572cb5b7d4b8c7d53a91d945bbc3f51f74441052
Richard P Sharp Jr <richpsharp@gmail.com>
2024-12-30 12:11:56 -0800

    fixed upt he run_container.bat so that works

--- run_container.bat
+++ run_container.bat
@@ -1,81 +1,25 @@
 @echo on
+
+:: Set the Docker image name and work directory
 set "IMAGE_NAME=esoc_c_models"
 set "WORKDIR=/usr/local/esoc_c_models"
+
+:: Ensure at least one argument is provided
+if "%1"=="" (
+    echo "Error: Please provide the Python script to run as the first argument."
+    exit /b 1
+)
+
+:: Get the script path (first argument)
+set "SCRIPT_PATH=%1"
+
+:: Shift the arguments to pass the rest to Docker
+shift
+set "SCRIPT_ARGS=%*"
+
+:: Build the Docker image
 docker build -t %IMAGE_NAME% .
 
-if errorlevel 1 (
-    echo Docker build failed. Exiting...
-    exit /b 1
-)
-
-for /f "delims=" %%i in ('cd') do set "CURRENT_DIR=%%i"
-set "CURRENT_DIR=%CURRENT_DIR:\=/%
-
-:: Array of scripts
-set "SCRIPTS=
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/swy_global/run_swy_global.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/pollination_sufficiency/pollination_pipeline.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/distance-to-hab-with-friction/ppl_travel_coverage.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/distance-to-hab-with-friction/mask_travel_coverage.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/people_protected_by_coastal_habitat/people_protected_by_coastal_habitat.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/coastal_risk_reduction/coastal_risk_reduction.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/global_flow_accumulation.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/downstream_beneficiaries.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/downstream_mask.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/ndr_sdr_global/run_ndr_sdr_pipeline.py --help
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-if errorlevel 1 (
-    echo Error occurred while running %%SCRIPT --help. Exiting...
-    exit /b 1
-)
-
-echo All scripts executed successfully.
+:: Map the current directory to the container's work directory and run the Python script with all additional arguments
+set "CURRENT_DIR=%CD%"
+docker run -it --rm --env WORKDIR=%WORKDIR% -v "%CURRENT_DIR%:%WORKDIR%/data" %IMAGE_NAME% micromamba run -n hf39 python %SCRIPT_PATH% %SCRIPT_ARGS%
