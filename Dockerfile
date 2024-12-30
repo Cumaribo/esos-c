81ff486042c42e3dbdc315bad33451941b5f8383
Richard P Sharp Jr <richpsharp@gmail.com>
2024-12-30 12:04:01 -0800

    track the environment and the updated docker container

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,38 @@
-# Container ESOS-C model runs
-FROM ubuntu:jammy
+FROM mambaorg/micromamba:1.4.2-bullseye
 
-RUN apt-get update -qq && \
-    apt-get install -y \
-    emacs \
-    libspatialindex-dev \
-    python3-pip \
-    postgresql-client \
-    openssl \
-    curl \
-    git \
-    gdal-bin \
-    python3-gdal
+# We want all RUN commands to use Bash.
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+# Copy over your environment file
+COPY environment.yml /tmp/environment.yml
+
+# Create the environment
+RUN micromamba create -n hf39 -f /tmp/environment.yml --yes && \
+    micromamba clean --all --yes
 
 ARG WORKDIR=/usr/local/esoc_c_models
 ENV WORKDIR=${WORKDIR}
+
+RUN micromamba shell init -s bash -p /opt/conda
+
+# If needed, ensure the file exists and append your activation line
+RUN touch /home/mambauser/.bashrc
+RUN echo 'micromamba activate hf39' >> /home/mambauser/.bashrc
+
+USER root
+RUN apt-get update -y
+RUN apt install git -y
+RUN apt-get install -y --no-install-recommends \
+    gcc \
+    g++ \
+    make \
+    build-essential \
+    zlib1g-dev \
+    libpython3-dev \
+    libssl-dev \
+    libffi-dev \
+    libsqlite3-dev \
+    libgdal-dev
 
 RUN git clone https://github.com/springinnovate/swy_global.git ${WORKDIR}/swy_global
 RUN git clone https://github.com/springinnovate/ndr_sdr_global.git ${WORKDIR}/ndr_sdr_global
@@ -24,27 +42,14 @@ RUN git clone https://github.com/springinnovate/people_protected_by_coastal_habi
 RUN git clone https://github.com/springinnovate/distance-to-hab-with-friction ${WORKDIR}/distance-to-hab-with-friction
 RUN git clone https://github.com/springinnovate/pollination_sufficiency ${WORKDIR}/pollination_sufficiency
 
-RUN pip3 install cython
-
 RUN git clone https://github.com/springinnovate/ecoshard.git /usr/local/ecoshard && \
     cd /usr/local/ecoshard && \
-    pip3 install .
+    micromamba run -n hf39 pip install .
 
 RUN git clone https://github.com/springinnovate/inspring.git /usr/local/inspring && \
     cd /usr/local/inspring && \
-    pip3 install .
+    micromamba run -n hf39 pip install .
 
-RUN pip3 install \
-    geopandas \
-    shapely \
-    pyproj \
-    psutil \
-    scipy \
-    requests \
-    numexpr
-
-# Create the data directory
-RUN mkdir -p ${WORKDIR}/data
-
+USER mambauser
 WORKDIR ${WORKDIR}
-CMD ["bash"]
+CMD ["/bin/bash"]
--- run_container.bat
+++ run_container.bat
@@ -11,4 +11,71 @@ if errorlevel 1 (
 for /f "delims=" %%i in ('cd') do set "CURRENT_DIR=%%i"
 set "CURRENT_DIR=%CURRENT_DIR:\=/%
 
-docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME%
+:: Array of scripts
+set "SCRIPTS=
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/swy_global/run_swy_global.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/pollination_sufficiency/pollination_pipeline.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/distance-to-hab-with-friction/ppl_travel_coverage.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/distance-to-hab-with-friction/mask_travel_coverage.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/people_protected_by_coastal_habitat/people_protected_by_coastal_habitat.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/coastal_risk_reduction/coastal_risk_reduction.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/global_flow_accumulation.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/downstream_beneficiaries.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/downstream-beneficiaries/downstream_mask.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+docker run -it --rm --env WORKDIR=%WORKDIR% -v %CURRENT_DIR%:%WORKDIR%/data %IMAGE_NAME% python3  /usr/local/esoc_c_models/ndr_sdr_global/run_ndr_sdr_pipeline.py --help
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+if errorlevel 1 (
+    echo Error occurred while running %%SCRIPT --help. Exiting...
+    exit /b 1
+)
+
+echo All scripts executed successfully.
